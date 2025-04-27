2261621902f6f2796c37bcc548b872f7503ea64a
Mohamed Saqr <mohamedsaqrr75@gmail.com>
2025-04-28 00:31:20 +0300

    create featured books cubit states

--- lib/core/utils/api_service.dart
+++ lib/core/utils/api_service.dart
@@ -1,6 +1,4 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:dio/dio.dart';
-
 class ApiService {
   final _baseUrl = 'https://www.googleapis.com/books/v1/';
   final Dio _dio;
